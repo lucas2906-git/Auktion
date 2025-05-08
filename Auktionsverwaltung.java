97fe9adfb311414a20b87c4a255d53dcaa09d59a
Fabske1893 <fabi.gross2004@gmail.com>
2025-05-08 16:16:26 +0200

    Auktionsverwaltung.java
    Javadoc

--- Auktionsverwaltung.java
+++ Auktionsverwaltung.java
@@ -1,23 +1,44 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Verwaltet die Logik für das Erstellen und Starten von Auktionen.
+ * Verwaltet alle laufenden und erstellten Auktionen innerhalb des Auktionshauses.
+ * Zuständig für das Erzeugen neuer Auktionen und deren Start in eigenen Threads.
  */
 public class Auktionsverwaltung {
+
     private List<Auktion> auktionen = new ArrayList<>();
 
+    /**
+     * Erstellt eine neue Auktion mit dem gegebenen Artikel, Auktionator und der Liste von Bietern.
+     * Die Auktion wird zur internen Liste hinzugefügt.
+     *
+     * @param item         Der zu versteigernde Artikel
+     * @param auktionator  Der Auktionator (Verkäufer)
+     * @param bieter       Die Liste der teilnehmenden Bieter
+     * @return Die erstellte Auktion
+     */
     public Auktion erstelleAuktion(Item item, User auktionator, List<User> bieter) {
         Auktion auktion = new Auktion(item, auktionator, bieter);
         auktionen.add(auktion);
         return auktion;
     }
 
+    /**
+     * Startet die übergebene Auktion in einem neuen Thread.
+     * Die Auktion läuft damit parallel und autonom.
+     *
+     * @param auktion Die zu startende Auktion
+     */
     public void starteAuktion(Auktion auktion) {
         new Thread(auktion).start();
     }
 
+    /**
+     * Gibt eine Liste aller bisher erstellten Auktionen zurück.
+     *
+     * @return Die Liste der Auktionen
+     */
     public List<Auktion> getAuktionen() {
         return auktionen;
     }
